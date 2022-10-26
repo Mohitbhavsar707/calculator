f684a3893af160e2a63274745be586c3072cc4b5
Mohit Bhavsar <64053968+Mohitbhavsar707@users.noreply.github.com>
2022-10-26 01:19:16 -0400

    Added Files

---
+++ src/components/Title.js
@@ -0,0 +1,23 @@
+// import React from 'react'
+// import './Title.css';
+
+
+// function Title() {
+//   return (
+//     <div>Title</div>
+//   )
+// }
+
+// export default Title
+
+import React from 'react'
+
+function Title() {
+  return (
+    <div className="Title">
+      <h1>MOHIT BHAVSAR CALCULATOR APP </h1>
+    </div>
+  )
+}
+
+export default Title
